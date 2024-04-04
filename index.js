360c588b57e20f87853393ad41c03568a6c9ea5d
Gaurab Lohani <132120369+gaurablohani01@users.noreply.github.com>
2024-04-04 10:26:58 +0545

    Update index.js

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ function updateTime() {
     let ampm = hour >= 12 ? 'PM' : 'AM';
     hour = hour % 12;
     if (hour == 0) {
-        document.querySelector('.date').innerHTML = `${months[month]} ${monthdate} || '12':${min} ${ampm}`;
+        document.querySelector('.date').innerHTML = ` ${months[month]} ${monthdate} || '12':${min} ${ampm}`;
     } else {
-        document.querySelector('.date').innerHTML = `${months[month]} ${monthdate} || ${hour}:${min} ${ampm}`;
+        document.querySelector('.date').innerHTML = ` ${months[month]} ${monthdate} || ${hour}:${min} ${ampm}`;
     }
    
 
     let country = new Intl.DateTimeFormat().resolvedOptions().timeZone;
-    document.querySelector('.year').innerHTML = `${year} - ${country}`;
-    document.querySelector('.cuurentyear').innerHTML = `${year}`;
+    document.querySelector('.year').innerHTML = ` ${year} - ${country}`;
+    document.querySelector('.cuurentyear').innerHTML = ` ${year}`;
 }
 
 updateTime();
