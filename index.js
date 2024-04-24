92150c5bcdba5c057e491027efdf92bf1ed2fecd
Gaurab Lohani <132120369+gaurablohani01@users.noreply.github.com>
2024-04-24 19:29:32 +0545

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
                 let date = new Date();
                 let hour = date.getHours();
                 let min = date.getMinutes();
+                let sec=   date.getSeconds();
                 let month = date.getMonth();
                 let monthdate = date.getDate();
                 let year = date.getFullYear();
@@ -10,9 +11,9 @@
                 let ampm = hour >= 12 ? 'PM' : 'AM';
                 hour = hour % 12;
                 if (hour == 0) {
-                    document.querySelector('.date').innerHTML = `${months[month]} ${monthdate} || 12:${min} ${ampm}`;
+                    document.querySelector('.date').innerHTML = `${months[month]} ${monthdate} || 12:${min}:{sec} ${ampm}`;
                 } else {
-                    document.querySelector('.date').innerHTML = `${months[month]} ${monthdate} || ${hour}:${min} ${ampm}`;
+                    document.querySelector('.date').innerHTML = `${months[month]} ${monthdate} || ${hour}:${min}{sec} ${ampm}`;
                 }
                 
                 let country = new Intl.DateTimeFormat().resolvedOptions().timeZone;
